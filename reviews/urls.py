34a57d5fecb0cee0305f2e4536972934d9de3b59
Neeraj-Palliyali <palliyali.neeraj@gmail.com>
2021-11-17 18:22:36 +0530

    meta eroor

--- reviews/urls.py
+++ reviews/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 from . import views
+import reviews
 
 urlpatterns = [
-    path("", views.review),
-    path("thank_you", views.thank_you)
+    path("", views.ReviewView.as_view()),
+    path("thank_you", views.ThankYouView.as_view()),
+    path("reviews", views.ReviewsListView.as_view()),
+    path("reviews/<int:pk>", views.SingleReviewView.as_view())
 ]
--- reviews/views.py
+++ reviews/views.py
@@ -1,24 +1,34 @@
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
-from .forms import ReviewForm
+from django.views import View
+from django.views.generic import TemplateView, ListView, DetailView
+from django.views.generic.edit import CreateView
 
+from .forms import ReviewForm
+from .models import Review
 # Create your views here.
 
 
-def review(request):
-    if(request.method == 'POST'):
-        form = ReviewForm(request.POST)
-        if(form.is_valid()):
-            form.save()
-            return HttpResponseRedirect("/thank_you")
+class ReviewView(CreateView):
+    model = Review
+    fields = ReviewForm
+    template_name = "reviews/review.html"
+    success_url = "/thank_you"
+class ThankYouView(TemplateView):
+    template_name = "reviews/thankyou.html"
 
-    else:
-        form = ReviewForm()
-
-    return render(request, "reviews/review.html", {
-        "form": form
-    })
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["message"]="Works properly"
+        return context
 
 
-def thank_you(request):
-    return render(request, "reviews/thankyou.html")
+class ReviewsListView(ListView):
+    template_name = "reviews/review_list.html"
+    model = Review
+    context_object_name="reviews"
+
+class SingleReviewView(DetailView):
+    template_name = "reviews/single_review.html"
+
+    model = Review
